57c8da472a8122eea88d7338a61e4e4b860b24d5
Sonnb1999 <103470213+Sonnb1999@users.noreply.github.com>
2023-11-30 23:36:28 +0700

    update

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,19 +7,19 @@
     'version': '16.0.1.2',
     'license': 'LGPL-3',
     'support ': 'Dev.Phuong2711@gmail.com',
-    'depends': ['base', 'contacts'],
+    'depends': ['base', 'contacts', 'web'],
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/menu.xml',
     ],
     'assets': {
-        # 'web.assets_backend': [
-        #     'th_pagoda/static/src/js/*.js',
-        #     'th_pagoda/static/src/xml/*.xml',
-        #     'th_pagoda/static/src/scss/*.scss',
-        #
-        # ],
+        'web.assets_backend': [
+            'estate/static/src/js/*.js',
+            # 'th_pagoda/static/src/xml/*.xml',
+            # 'th_pagoda/static/src/scss/*.scss',
+
+        ],
     },
     'installable': True,
     'application': True,
